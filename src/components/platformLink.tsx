fe0d7fe102697f1e26ff697261e6d5832f997967
Tor Arne Vestbø <torarnv@gmail.com>
2021-04-08 13:20:37 +0200

    Allow overriding platform for PlatformLink

--- src/components/platformLink.tsx
+++ src/components/platformLink.tsx
@@ -5,11 +5,12 @@ import SmartLink from "./smartLink";
 
 type Props = {
   to?: string;
+  platform?: string;
   children: JSX.Element;
 };
 
-export default ({ children, to }: Props): JSX.Element => {
-  const [currentPlatform] = usePlatform(null);
+export default ({ children, to, platform }: Props): JSX.Element => {
+  const [currentPlatform] = usePlatform(platform);
   let path = currentPlatform ? currentPlatform.url : `/platform-redirect/`;
   if (to)
     path += currentPlatform ? to.slice(1) : `?next=${encodeURIComponent(to)}`;
